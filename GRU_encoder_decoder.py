5a48bc7213e71490238682b39027e2d8ecdfe2bb
Luka Perovic <lukarajkin@gmail.com>
2023-08-23 18:49:31 +0200

    add efficient translate, add custom dropout

--- GRU_encoder_decoder.py
+++ GRU_encoder_decoder.py
@@ -10,15 +10,46 @@ import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
+import time
 from tensorflow import keras
+from keras import backend as K
 from keras.models import Model
-from keras.layers import Input, Dense, Embedding, GRU, Bidirectional, Concatenate, Dropout
+from keras.layers import Input, Dense, Embedding, GRU, Bidirectional, Concatenate, Dropout, Layer, Lambda, Masking
 from keras.utils import pad_sequences
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk import edit_distance
 
+class CustomDropout(Layer):
+    def __init__(self, custom_value, rate, **kwargs):
+        super(CustomDropout, self).__init__(**kwargs)
+        self.custom_value = custom_value
+        self.rate = rate
 
+    def call(self, inputs, training=None):
+        if training is None:
+            training = K.learning_phase()
+    
+        if training:
+            def dropped_inputs():
+                return tf.where(tf.logical_and(
+                    tf.random.uniform(shape=tf.shape(inputs)) < self.rate,
+                    inputs != 0),  # Masking zeros
+                    self.custom_value,
+                    inputs)
+            return K.in_train_phase(dropped_inputs, inputs, training=training)
+        return inputs
+
+        return tf.keras.backend.in_train_phase(dropped_inputs, inputs, training=training)
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "custom_value": self.custom_value,
+            "rate": self.rate
+        })
+        return config
 
 def clean_texts(data_frame):
     input_texts = data_frame['translation']
@@ -188,24 +219,46 @@ encoder_input_data_val, decoder_input_data_val, decoder_output_data_val = create
 latent_dim = 256
 
 encoder_input_tensor = Input(shape = (input_pad_len, ))
-encoder_embedding = Embedding(input_dim = num_input_words + 1, output_dim = embedding_size, mask_zero = True, weights = [input_embedding_matrix], trainable = True)(encoder_input_tensor)
+modified_input = CustomDropout(1.0, 0.05)(encoder_input_tensor)
+encoder_embedding = Embedding(input_dim = num_input_words + 1, output_dim = embedding_size, mask_zero = True, weights = [input_embedding_matrix], trainable = True)(modified_input)
 _, forward_state, backward_state = Bidirectional(GRU(units = latent_dim, return_state = True, dropout = 0.5))(encoder_embedding)
 state_h = Concatenate(axis=-1)([forward_state, backward_state])
-#_, state_h = GRU(units = latent_dim, return_state = True, unroll = False)(encoder_embedding)
-#encoder_model = Model(encoder_input_tensor, encoder_output)
+
+encoder_model = Model(encoder_input_tensor, state_h)
+
+# =============================================================================
+# decoder_input_tensor = Input(shape = (target_pad_len, ))
+# decoder_embedding = Embedding(input_dim = num_target_words + 1, output_dim = embedding_size, mask_zero = True, weights = [target_embedding_matrix], trainable = True)(decoder_input_tensor)
+# decoder_outputs = GRU(units = latent_dim*2, return_sequences = True, dropout = 0.5)(decoder_embedding, initial_state = state_h)
+# output = Dropout(0.5)(decoder_outputs)
+# output = Dense(units = num_target_words + 1, activation = 'softmax')(decoder_outputs)
+# 
+# model_gru = Model(inputs = [encoder_input_tensor, decoder_input_tensor], outputs = output)
+# model_gru.summary()
+# =============================================================================
 
 decoder_input_tensor = Input(shape = (target_pad_len, ))
-decoder_embedding = Embedding(input_dim = num_target_words + 1, output_dim = embedding_size, mask_zero = True, weights = [target_embedding_matrix], trainable = True)(decoder_input_tensor)
-decoder_outputs, _, = GRU(units = latent_dim*2, return_sequences = True, return_state = True, dropout = 0.5)(decoder_embedding, initial_state = state_h)
+decoder_embedding_layer = Embedding(input_dim = num_input_words + 1, output_dim = embedding_size, mask_zero = True, weights = [input_embedding_matrix], trainable = True)
+decoder_embedding = decoder_embedding_layer(decoder_input_tensor)
+decoder_GRU_layer = GRU(units = latent_dim*2, return_sequences = True, return_state = True, dropout = 0.5)
+decoder_outputs, _ = decoder_GRU_layer(decoder_embedding, initial_state = state_h)
 output = Dropout(0.5)(decoder_outputs)
-output = Dense(units = num_target_words + 1, activation = 'softmax')(decoder_outputs)
+decoder_dense_layer = Dense(units = num_target_words + 1, activation = 'softmax')
+output = decoder_dense_layer(output)
 
 model_gru = Model(inputs = [encoder_input_tensor, decoder_input_tensor], outputs = output)
-#model_gru.summary()
-    
+model_gru.summary()
+
+decoder_input_tensor = Input(shape = (1,))
+decoder_state = Input(shape = (latent_dim*2,))
+decoder_outputs, decoder_state_new = decoder_GRU_layer(decoder_embedding_layer(decoder_input_tensor), initial_state = decoder_state)
+output = decoder_dense_layer(decoder_outputs)
+
+decoder_model = Model(inputs = [decoder_input_tensor, decoder_state], outputs = [output, decoder_state_new])
+   
 model_gru.compile(optimizer = Adam(0.0002), loss = 'sparse_categorical_crossentropy', metrics = ['acc'])
 batch_size = 128
-epochs = 5
+epochs = 50
 
 checkpoint = ModelCheckpoint('best_model_weights.h5', save_best_only=True, save_weights_only=True, monitor='val_loss', mode='min')
 
@@ -223,7 +276,9 @@ history = model_gru.fit([encoder_input_data, decoder_input_data], decoder_output
 #512 latend_dim, ostalo isto, 0.72 val_loss
 #256, promenjen target_pad_len na fiksnih 60, 0.433 val_loss, 36/60 * 0.712 = 0.427 za referencu
 #zaseban validacioni, nepoznate reci na praznine, 0.364, praznine ignorise kad racuna loss
-#zaseban validacioni, mapiranje reci koje se jednom pojavljuju na <Unknown>
+#zaseban validacioni, mapiranje reci koje se jednom pojavljuju na <Unknown>, 0.364 val_loss
+#nakon 0.05 dropouta na <Unknown> nad inputima, 0.358 val_loss, sporije dosta konvergira, treba oko 80 epoha
+
 
 model_gru.load_weights('best_model_weights.h5')
 
@@ -261,6 +316,43 @@ def translate2(input_sentence):
             decoder_input[0, i+1] = next_word
     return ' '.join(inverted_target_word_index[num] for num in output_sentence).replace(' - ', '-')
 
+#Brzi translate
+def translate3(input_sentences):
+    input_sentences = [input_sentence.replace('.', '').replace(',', '').replace('!','').replace('"','').replace('?','').lower() for input_sentence in input_sentences]
+    data_length = len(input_sentences)
+    target_placeholder_sentences = ['']*data_length
+    encoder_input_data, _, _ = create_model_data(input_sentences, target_placeholder_sentences, input_word_index, target_word_index, input_pad_len, target_pad_len)
+    decoder_input_data = np.zeros((data_length, target_pad_len))
+    decoder_input_data[:, 0] = np.zeros(data_length) + target_word_index['<Start>']
+    decoder_output = np.zeros((data_length, input_pad_len))
+    for i in range(target_pad_len-1):
+        decoder_output = model_gru.predict([encoder_input_data, decoder_input_data], verbose = 0)
+        next_words = np.argmax(decoder_output[:, i, :], axis = -1)
+        decoder_input_data[:, i+1] = next_words 
+    output_sentences = [' '.join(inverted_target_word_index[num] for num in output_sentence) for output_sentence in decoder_input_data[:,1:]]
+    output_sentences = [output_sentence.split('<End>',1)[0].replace(' - ','-') for output_sentence in output_sentences]
+    return output_sentences
+
+#Jos brzi
+def translate4(input_sentences):
+    input_sentences = [input_sentence.replace('.', '').replace(',', '').replace('!','').replace('"','').replace('?','').lower() for input_sentence in input_sentences]
+    data_length = len(input_sentences)
+    target_placeholder_sentences = ['']*data_length
+    encoder_input_data, _, _ = create_model_data(input_sentences, target_placeholder_sentences, input_word_index, target_word_index, input_pad_len, target_pad_len)
+    encoder_final_state = encoder_model.predict(encoder_input_data, verbose = 0)
+    decoder_input_data = np.zeros((data_length, 1)) + target_word_index['<Start>']
+    decoder_state = encoder_final_state
+    decoder_output = np.zeros((data_length, target_pad_len - 1))
+    for i in range(target_pad_len - 1):
+        decoder_output_temp, decoder_state = decoder_model.predict([decoder_input_data, decoder_state], verbose = 0)
+        next_words = np.argmax(decoder_output_temp, axis = -1)
+        decoder_input_data = next_words
+        decoder_output[:, i] = next_words.reshape((data_length,))
+    output_sentences = [' '.join(inverted_target_word_index[num] for num in output_sentence) for output_sentence in decoder_output]
+    output_sentences = [output_sentence.split('<End>',1)[0].replace(' - ','-') for output_sentence in output_sentences]
+    return output_sentences
+
+
 test_input_sentences = df_test['translation']
 test_references = df_test['orth']
 umlaut_dict = {'AE': 'Ä',
@@ -268,14 +360,42 @@ umlaut_dict = {'AE': 'Ä',
                'UE': 'Ü'}
 for key in umlaut_dict.keys():
     test_references = [text.replace(key, umlaut_dict[key]) for text in test_references]
+
+print(test_input_sentences[0])
+print(translate4(test_input_sentences[0]))
+print(test_references[0])
+print(sentence_bleu([test_references[0].split()], translate2(test_input_sentences[0]).split(), smoothing_function = SmoothingFunction().method1))
 translations = [translate2(input_sentence) for input_sentence in test_input_sentences] #Mnogo sporo, mora da se napravi efikasna verzija
-bleu_scores = [sentence_bleu(reference.split(), translation.split()) for reference, translation in zip(test_references, translations)]
-bleu_final = np.mean(bleu_scores) #Ne radi lepo, daje samo nule.
+bleu_scores = [sentence_bleu([reference.split()], translation.split(), smoothing_function = SmoothingFunction().method7) for reference, translation in zip(test_references, translations)]
+smooth_bleu_final = np.mean(bleu_scores) #Mora smoothing da se stavi, inace izbacuje samo nule
+wer = np.mean([edit_distance(reference.split(), translation.split())/len(reference.split()) for reference, translation in zip(test_references, translations)])
     
+# =============================================================================
+# start_time = time.time()
+# translation2 = [translate2(input_sentence) for input_sentence in test_input_sentences]
+# end_time = time.time()
+# print("Elapsed time for translation 2: ", end_time - start_time) #612s
+# 
+# start_time = time.time()
+# translation3 = translate3(test_input_sentences)
+# end_time = time.time()
+# print("Elapsed time for translation 3: ", end_time - start_time) #96s
+#
+# start_time = time.time()
+# translation4 = translate4(test_input_sentences)
+# end_time = time.time()
+# print("Elapsed time for translation 3: ", end_time - start_time) #15s
+# =============================================================================
 
-    
-    
 
 
+
+dropout_input = Input(shape = (input_pad_len,))
+output = CustomDropout(1.0, 0.5)(dropout_input)
+output = Embedding(input_dim = num_input_words + 1, output_dim = embedding_size, mask_zero = True, weights = [input_embedding_matrix], trainable = False)(output)
+
+dropout_model = Model(dropout_input, output)
+
+dropout_model(encoder_input_data, training = True)
 
                
